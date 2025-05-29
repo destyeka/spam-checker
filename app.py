076a6fd77a4f45080b521df3ab68bd0fa23c338f
destyeka <143570602+destyeka@users.noreply.github.com>
2025-05-29 14:59:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ import gdown
 # Shareable link (make sure it's public or "anyone with the link")
 gdown.download("https://drive.google.com/file/d/131e-hQWCBBYvqmuuVSGF_nwxCw9grcSg/view?usp=drive_link", output="model.zip", quiet=False)
 
-# Load model and tokenizer
-# @st.cache_resource
-# def load_model():
-#     tokenizer = AutoTokenizer.from_pretrained("path/to/saved_tokenizer")
-#     model = AutoModelForSequenceClassification.from_pretrained("path/to/saved_model")
-#     return tokenizer, model
-
-# tokenizer, model = load_model()
 
 st.title("📮 Spam Message Classifier")
 st.write("Type a message below and let BERT decide if it's *spam* or *ham* 🕵️‍♀️📩")
@@ -25,19 +17,6 @@ if st.button("Classify"):
     if text.strip() == "":
         st.warning("Please enter a message, babe 😘")
     else:
-        # Tokenize input
-        # inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
-        
-        # Ensure model runs on the correct device
-        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # model.to(device)
-        # inputs = {k: v.to(device) for k, v in inputs.items()}
-
-        # # Predict
-        # with torch.no_grad():
-        #     outputs = model(**inputs)
-        #     probs = torch.softmax(outputs.logits, dim=1)
-        #     pred = torch.argmax(probs, dim=1).item()
 
         label = "💔 Spam" if pred == 1 else "💌 Not Spam"
         st.subheader(f"Prediction: {label}")
